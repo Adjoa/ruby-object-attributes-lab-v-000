85641d3d1572f2f80fcb41ca78df338c5f6edbf6
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-17 15:54:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,12 @@
 class Dog
 
-  def name=(new_name)
-    @name = new_name
+  def name=(name)
+    @name = name
   end
 
-  # def name=(name)
-  #   @name = name
-  # end
-  #
   def name
     @name
   end
 
+  def breed=(breed)
 end
